300db94badde004034fdf818954aeddf8da676c3
nathanwag15 <65185723+nathanwag15@users.noreply.github.com>
2022-07-14 23:15:12 -0600

    moment integration

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
+import Moment from "react-moment";
+import moment from "moment";
 
 export default function Home() {
+  const current = new Date();
+  const currentDate = moment().format("dddd, MMM Do YY");
+  const tomorrowDate = moment().add(1, "days").calendar("dddd");
+  const threeDayDate = moment().add(2, "days").calendar("dddd");
+  const fourDayDate = moment().add(2, "days").calendar("dddd");
+  const fiveDayDate = moment().add(2, "days").calendar("dddd");
+  const sixDayDate = moment().add(2, "days").calendar("dddd");
+  const sevenDayDate = moment().add(2, "days").calendar("D");
   return (
     <div className=" week-page-wrapper bg-white">
       <div className="text-3xl font-bold text-dark-blue">Week in Review</div>
       <div className="week-wrapper">
         <div className="day-wrapper border-2 border-solid border-dark-blue">
-          <div className="day-title text-2xl text-medium-blue">day</div>
+          <div className="day-title text-xl text-medium-blue">
+            {currentDate}
+          </div>
           <div className="day-todo-list">
             <div>eat chicken</div>
             <div>eat chicken</div>
@@ -18,7 +30,9 @@ export default function Home() {
           </div>
         </div>
         <div className="day-wrapper border-2 border-solid border-dark-blue">
-          <div className="day-title text-2xl text-medium-blue">day</div>
+          <div className="day-title text-2xl text-medium-blue">
+            {tomorrowDate}
+          </div>
           <div className="day-todo-list">
             <div>eat chicken</div>
             <div>eat chicken</div>
@@ -28,7 +42,9 @@ export default function Home() {
           </div>
         </div>
         <div className="day-wrapper border-2 border-solid border-dark-blue">
-          <div className="day-title text-2xl text-medium-blue">day</div>
+          <div className="day-title text-2xl text-medium-blue">
+            {threeDayDate}
+          </div>
           <div className="day-todo-list">
             <div>eat chicken</div>
             <div>eat chicken</div>
@@ -38,7 +54,9 @@ export default function Home() {
           </div>
         </div>
         <div className="day-wrapper border-2 border-solid border-dark-blue">
-          <div className="day-title text-2xl text-medium-blue">day</div>
+          <div className="day-title text-2xl text-medium-blue">
+            {fourDayDate}
+          </div>
           <div className="day-todo-list">
             <div>eat chicken</div>
             <div>eat chicken</div>
@@ -48,7 +66,9 @@ export default function Home() {
           </div>
         </div>
         <div className="day-wrapper border-2 border-solid border-dark-blue">
-          <div className="day-title text-2xl text-medium-blue">day</div>
+          <div className="day-title text-2xl text-medium-blue">
+            {fiveDayDate}
+          </div>
           <div className="day-todo-list">
             <div>eat chicken</div>
             <div>eat chicken</div>
@@ -58,7 +78,9 @@ export default function Home() {
           </div>
         </div>
         <div className="day-wrapper border-2 border-solid border-dark-blue">
-          <div className="day-title text-2xl text-medium-blue">day</div>
+          <div className="day-title text-2xl text-medium-blue">
+            {sixDayDate}
+          </div>
           <div className="day-todo-list">
             <div>eat chicken</div>
             <div>eat chicken</div>
@@ -68,7 +90,9 @@ export default function Home() {
           </div>
         </div>
         <div className="day-wrapper border-2 border-solid border-dark-blue">
-          <div className="day-title text-2xl text-medium-blue">day</div>
+          <div className="day-title text-2xl text-medium-blue">
+            {sevenDayDate}
+          </div>
           <div className="day-todo-list">
             <div>eat chicken</div>
             <div>eat chicken</div>
